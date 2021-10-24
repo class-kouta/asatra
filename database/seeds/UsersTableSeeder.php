b64cd4e8ec1aa59336c61d80f89e0045b6de3d9d
class-kouta <ff03csm26@gmail.com>
2021-10-24 10:20:17 +0900

    ダミーデータの追加

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,16 +13,89 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
-                'name' => '康太',
-                'sex' => '1',
-                'profile_image' => '',
-                'email' => 'kouta@gmail.com',
+                'name' => 'ゆい',
+                'sex' => '2',
+                'email' => 'yui@gmail.com',
                 'password' => Hash::make('password'),
             ],[
-                'name' => 'あゆみ',
+                'name' => 'chisako',
                 'sex' => '2',
-                'profile_image' => '',
-                'email' => 'ayumi@gmail.com',
+                'email' => 'chisako@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'Y.K',
+                'sex' => '1',
+                'email' => 'y.k@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'さこた',
+                'sex' => '1',
+                'email' => 'sakota@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'ハジ',
+                'sex' => '1',
+                'email' => 'hazi@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'miyu',
+                'sex' => '2',
+                'email' => 'miyu@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'まー',
+                'sex' => '2',
+                'email' => 'maa@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'こんた',
+                'sex' => '1',
+                'email' => 'konta@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'ayu',
+                'sex' => '2',
+                'email' => 'ayu@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'りくママ',
+                'sex' => '2',
+                'email' => 'rikumama@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'RYU',
+                'sex' => '1',
+                'email' => 'ryu@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'さかな',
+                'sex' => '2',
+                'email' => 'sakana@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'まいか',
+                'sex' => '2',
+                'email' => 'maika@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'そのか',
+                'sex' => '2',
+                'email' => 'sonoka@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'こーた',
+                'sex' => '1',
+                'email' => 'kota@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'mimi',
+                'sex' => '2',
+                'email' => 'mimi@gmail.com',
+                'password' => Hash::make('password'),
+            ],[
+                'name' => 'ばろり',
+                'sex' => '1',
+                'email' => 'barori@gmail.com',
                 'password' => Hash::make('password'),
             ]
         ]);
