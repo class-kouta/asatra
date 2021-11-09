2feb34e1b8987c4e6225d4ae6a8ca91bb073728a
class-kouta <ff03csm26@gmail.com>
2021-11-09 19:58:53 +0900

    ヘッダーの変更

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -21,46 +21,63 @@
 </head>
 <body>
     <div class="container">
-        <a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
-        <br><br>
 
-        @guest
-            <li><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
-            @if (Route::has('register'))
-                <li><a href="{{ route('register') }}">{{ __('Register') }}</a></li>
-            @endif
-        @else
-            <p>{{ Auth::user()->name }}</p>
-            <a href="{{ route('profile.edit') }}">プロフィール編集</a>
-            <br>
-            <a href="{{ route('posts.create') }}">追加</a>
-            <br>
-            <a href="{{ route('profile.myposts') }}">自分の投稿を見る</a>
-            <br>
-            <a href="{{ route('profile.myniceposts') }}">いいねした投稿を見る</a>
-            <br>
-            <a href="{{ route('profile.mycommentposts') }}">コメントした投稿を見る</a>
-            <br>
-            <a href="{{ route('profile.withdraw_confirm') }}">退会する</a>
-            <br>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            <div aria-labelledby="navbarDropdown">
-                <a  href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">アサーションって何？ <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">アサトレの使い方</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">おすすめの書籍</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">みんなの投稿</a>
+                    </li>
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">ログイン</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">新規登録</a>
+                        </li>
+                    @else
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('profile.edit') }}">ユーザ情報の変更</a>
+                                <a class="dropdown-item" href="{{ route('posts.create') }}">投稿する</a>
+                                <a class="dropdown-item" href="{{ route('profile.myposts') }}">自分の投稿</a>
+                                <a class="dropdown-item" href="{{ route('profile.myniceposts') }}">いいねした投稿</a>
+                                <a class="dropdown-item" href="{{ route('profile.mycommentposts') }}">コメントした投稿</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                    ログアウト
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
             </div>
-
-        @endguest
-    <div class="container">
+        </nav>
 
     <main class="py-4">
-      @yield('content')
+        @yield('content')
     </main>
+    </div>
 </body>
 </html>
