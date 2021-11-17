b66494c46396931e9f79249f266132f0600baf2c
class-kouta <ff03csm26@gmail.com>
2021-11-17 20:25:10 +0900

    login.blade.php, register.blade.php, edit.blade.php を bootstrap で整形

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,105 +4,103 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        {{-- 名前の入力欄 --}}
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">ニックネーム</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        {{-- 性別の入力欄 --}}
-                        <div class="form-group row">
-                            <label for="sex" class="col-md-4 col-form-label text-md-right">性別</label>
-                            <div class="col-md-6" style="padding-top: 8px">
-                                <select name="sex" class="form-select">
-                                    <option value="9">回答しない</option>
-                                    <option value="1">男性</option>
-                                    <option value="2">女性</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        {{-- 年代の入力欄 --}}
-                        <div class="form-group row">
-                            <label for="age" class="col-md-4 col-form-label text-md-right">年齢</label>
-                            <div class="col-md-6" style="padding-top: 8px">
-                                <select name="age" class="form-select">
-                                    <option value="0">回答しない</option>
-                                    <option value="10">10代</option>
-                                    <option value="20">20代</option>
-                                    <option value="30">30代</option>
-                                    <option value="40">40代</option>
-                                    <option value="50">50代</option>
-                                    <option value="60">60代〜</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        {{-- メールの入力欄 --}}
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        {{-- パスワードの入力欄 --}}
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        {{-- パスワード二回目の入力欄 --}}
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">パスワード（再確認）</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    新規登録
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div class="text-secondary border-bottom pl-3 pb-1 mb-5 h5">
+                新規登録
             </div>
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+
+                {{-- 名前の入力欄 --}}
+                <div class="form-group row">
+                    <label for="name" class="col-md-4 col-form-label text-md-right">ニックネーム</label>
+
+                    <div class="col-md-6">
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                {{-- メールの入力欄 --}}
+                <div class="form-group row">
+                    <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
+
+                    <div class="col-md-6">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                {{-- 性別の入力欄 --}}
+                <div class="form-group row mb-2">
+                    <label for="sex" class="col-md-4 col-form-label text-md-right">性別</label>
+                    <div class="col-md-6" style="padding-top: 8px">
+                        <select name="sex" class="">
+                            <option value="9">回答しない</option>
+                            <option value="1">男性</option>
+                            <option value="2">女性</option>
+                        </select>
+                    </div>
+                </div>
+
+                {{-- 年代の入力欄 --}}
+                <div class="form-group row mb-3">
+                    <label for="age" class="col-md-4 col-form-label text-md-right">年齢</label>
+                    <div class="col-md-6" style="padding-top: 8px">
+                        <select name="age" class="">
+                            <option value="0">回答しない</option>
+                            <option value="10">10代</option>
+                            <option value="20">20代</option>
+                            <option value="30">30代</option>
+                            <option value="40">40代</option>
+                            <option value="50">50代</option>
+                            <option value="60">60代〜</option>
+                        </select>
+                    </div>
+                </div>
+
+                {{-- パスワードの入力欄 --}}
+                <div class="form-group row">
+                    <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
+
+                    <div class="col-md-6">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                {{-- パスワード二回目の入力欄 --}}
+                <div class="form-group row">
+                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">パスワード（再確認）</label>
+
+                    <div class="col-md-6">
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                    </div>
+                </div>
+
+                <div class="form-group row mb-0">
+                    <div class="col-md-6 offset-md-4">
+                        <button type="submit" class="btn btn-primary">
+                            新規登録
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -2,51 +2,109 @@
 
 @section('content')
 <div class="container">
-    <form action="{{ route('profile.update') }}" method="post">
-    @csrf
-        ニックネーム<br>
-        <input type="text" name="name" value="{{ Auth::user()->name }}"><br>
-        @error('name')
-        <div class="error">{{ $message }}</div>
-        @enderror
-        <br>
+    <div class="row">
+        <div class="col-8" id="content">
 
-        メールアドレス<br>
-        <input type="email" name="email" value="{{ Auth::user()->email }}"><br>
-        @error('email')
-        <div class="error">{{ $message }}</div>
-        @enderror
-        <br>
+            <div class="text-secondary border-bottom pl-3 pb-1 mb-5 h5">
+                ユーザ情報の変更
+            </div>
 
-        性別<br>
-        <select name="sex" class="form-select">
-            <option value="9" {{ Auth::user()->sex === '9' ? 'selected' : '' }}>回答しない</option>
-            <option value="1" {{ Auth::user()->sex === '1' ? 'selected' : '' }}>男性</option>
-            <option value="2" {{ Auth::user()->sex === '2' ? 'selected' : '' }}>女性</option>
-        </select>
-        <br>
-        @error('sex')
-        <div class="error">{{ $message }}</div>
-        @enderror
-        <br>
+            <div class="ml-4">
+                <form action="{{ route('profile.update') }}" method="post" class="">
+                @csrf
+                    <div class="form-group mb-4">
+                        <div class="d-flex">
+                            <label for="name" class="text-secondary">ユーザ名</label>
+                            @error('name')
+                                <div class="text-danger ml-3">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <input type="text" name="name" class="form-control w-50" id="name" value="{{ Auth::user()->name }}">
+                    </div>
 
-        年代<br>
-        <select name="age" class="form-select">
-            <option value="0" {{ Auth::user()->age === 0 ? 'selected' : '' }}>回答しない</option>
-            <option value="10" {{ Auth::user()->age === 10 ? 'selected' : '' }}>10代</option>
-            <option value="20" {{ Auth::user()->age === 20 ? 'selected' : '' }}>20代</option>
-            <option value="30" {{ Auth::user()->age === 30 ? 'selected' : '' }}>30代</option>
-            <option value="40" {{ Auth::user()->age === 40 ? 'selected' : '' }}>40代</option>
-            <option value="50" {{ Auth::user()->age === 50 ? 'selected' : '' }}>50代</option>
-            <option value="60" {{ Auth::user()->age === 60 ? 'selected' : '' }}>60代〜</option>
-        </select>
-        <br><br>
-        @error('age')
-        <div class="error">{{ $message }}</div>
-        @enderror
+                    <div class="form-group mb-4">
+                        <div class="d-flex">
+                            <label for="email" class="text-secondary">メールアドレス</label>
+                            @error('email')
+                                <div class="text-danger ml-3">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <input type="email" name="email" class="form-control w-50" id="email" value="{{ Auth::user()->email }}">
+                    </div>
 
-        <button type="submit" class="btn btn-primary">更新</button>
+                    <div class="form-group mb-3">
+                        <label for="sex" class="text-secondary">性別</label>
+                        <select name="sex" id="sex" class="ml-3">
+                            <option value="9" {{ Auth::user()->sex === '9' ? 'selected' : '' }}>回答しない</option>
+                            <option value="1" {{ Auth::user()->sex === '1' ? 'selected' : '' }}>男性</option>
+                            <option value="2" {{ Auth::user()->sex === '2' ? 'selected' : '' }}>女性</option>
+                        </select>
+                        @error('sex')
+                            <div class="text-danger ml-3">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-    </form>
+                    <div class="form-group mb-4">
+                        <label for="age" class="text-secondary">年代</label>
+                        <select name="age" id="age" class="ml-3">
+                            <option value="0" {{ Auth::user()->age === 0 ? 'selected' : '' }}>回答しない</option>
+                            <option value="10" {{ Auth::user()->age === 10 ? 'selected' : '' }}>10代</option>
+                            <option value="20" {{ Auth::user()->age === 20 ? 'selected' : '' }}>20代</option>
+                            <option value="30" {{ Auth::user()->age === 30 ? 'selected' : '' }}>30代</option>
+                            <option value="40" {{ Auth::user()->age === 40 ? 'selected' : '' }}>40代</option>
+                            <option value="50" {{ Auth::user()->age === 50 ? 'selected' : '' }}>50代</option>
+                            <option value="60" {{ Auth::user()->age === 60 ? 'selected' : '' }}>60代〜</option>
+                        </select>
+                        @error('age')
+                            <div class="text-danger ml-3">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">更新</button>
+
+                    {{-- ニックネーム<br>
+                    <input type="text" name="name" value="{{ Auth::user()->name }}"><br>
+                    @error('name')
+                    <div class="error">{{ $message }}</div>
+                    @enderror
+                    <br>
+
+                    メールアドレス<br>
+                    <input type="email" name="email" value="{{ Auth::user()->email }}"><br>
+                    @error('email')
+                    <div class="error">{{ $message }}</div>
+                    @enderror
+                    <br>
+
+                    性別<br>
+                    <select name="sex" class="form-select">
+                        <option value="9" {{ Auth::user()->sex === '9' ? 'selected' : '' }}>回答しない</option>
+                        <option value="1" {{ Auth::user()->sex === '1' ? 'selected' : '' }}>男性</option>
+                        <option value="2" {{ Auth::user()->sex === '2' ? 'selected' : '' }}>女性</option>
+                    </select>
+                    <br>
+                    @error('sex')
+                    <div class="error">{{ $message }}</div>
+                    @enderror
+                    <br>
+
+                    年代<br>
+                    <select name="age" class="form-select">
+                        <option value="0" {{ Auth::user()->age === 0 ? 'selected' : '' }}>回答しない</option>
+                        <option value="10" {{ Auth::user()->age === 10 ? 'selected' : '' }}>10代</option>
+                        <option value="20" {{ Auth::user()->age === 20 ? 'selected' : '' }}>20代</option>
+                        <option value="30" {{ Auth::user()->age === 30 ? 'selected' : '' }}>30代</option>
+                        <option value="40" {{ Auth::user()->age === 40 ? 'selected' : '' }}>40代</option>
+                        <option value="50" {{ Auth::user()->age === 50 ? 'selected' : '' }}>50代</option>
+                        <option value="60" {{ Auth::user()->age === 60 ? 'selected' : '' }}>60代〜</option>
+                    </select>
+                    <br><br>
+                    @error('age')
+                    <div class="error">{{ $message }}</div>
+                    @enderror --}}
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
