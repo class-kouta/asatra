404e1e685b685050659ac1ad54345aa992739caa
class-kouta <ff03csm26@gmail.com>
2021-11-21 14:31:08 +0900

    ロゴを「アサトレ」に、微修正（レイアウトの修正）
    ○ 参照
    【Laravel】{{ config(‘app.name’, ‘Laravel’) }}について
    https://qiita.com/strrr/items/35e8f46573dd6770c295
    
    ○ 微修正
    ・nav の「アサーションとは？」の強調を消す
    ・準備中ページにプロフバーを設定

--- resources/views/nopage.blade.php
+++ resources/views/nopage.blade.php
@@ -2,10 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="ml-3 d-flex align-items-center">
-      <div>準備中...</div>
-      {{-- <a href="{{ url()->previous() }}">前のページに戻る</a> --}}
-      <button type="button" onClick="history.back()" class="btn btn-link">前のページに戻る</button>
+    <div class="row">
+        <div class="col-8">
+            <div class="ml-3 d-flex align-items-center">
+              <div>ページ作成中...</div>
+              <button type="button" onClick="history.back()" class="btn btn-link">前のページに戻る</button>
+            </div>
+        </div>
+
+        @component('components.profilebar')
+        @endcomponent
     </div>
 </div>
 @endsection
