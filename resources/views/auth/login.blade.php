ff140a5eeaa9aed5d899c87977a92469748ee249
class-kouta <ff03csm26@gmail.com>
2021-11-23 07:10:03 +0900

    リセットパスワードページを、一旦作成中ページに変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -58,7 +58,8 @@
                         </button>
 
                         @if (Route::has('password.request'))
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{-- <a class="btn btn-link" href="{{ route('password.request') }}"> --}}
+                            <a class="btn btn-link" href="{{ route('nopage') }}">
                                 パスワードを忘れた方へ
                             </a>
                         @endif
