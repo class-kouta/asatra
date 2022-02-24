b8a4d0c3b510acb3ea688d8aa05a4a62a039a52b
class-kouta <ff03csm26.en@gmial.com>
2022-02-25 06:59:02 +0900

    ロールバックの処理を追加
    カラム削除
    https://readouble.com/laravel/6.x/ja/migrations.html

--- database/migrations/2022_02_23_080139_add_status_to_posts_table.php
+++ database/migrations/2022_02_23_080139_add_status_to_posts_table.php
@@ -26,7 +26,7 @@ class AddStatusToPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropColumn('status');
         });
     }
 }
